a562c0ec9bb95d6de9d34b642c64a3f1bc7e5c48
menshikov <matveydrobot15@gmail.com>
2024-01-23 22:25:58 +0300

    Second commit

--- exercise 1/school/views.py
+++ exercise 1/school/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 
 from .models import Student
 
-
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    
+    # Получаем список всех учеников
+    students = Student.objects.all()
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # Передаем список учеников в контекст шаблона
+    context = {'students': students}
 
     return render(request, template, context)
